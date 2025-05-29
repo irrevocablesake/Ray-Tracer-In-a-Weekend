a8a704650a9889bcfaf4f995c83b9bf79df4adb6
Pratik Hadawale ~ sake :) <irrevocablesake@gmail.com>
2025-05-29 07:47:48 +0530

    FEATURE: Dielectric + material inside material

--- Material.h
+++ Material.h
@@ -80,4 +80,54 @@ class Metal : public Material {
         double fuzz;
 };
 
+class Dielectric : public Material {
+    public:
+
+        Dielectric( double refractionIndex ) : refractionIndex( refractionIndex ) { }
+
+        Vector3 refract( const Vector3 &vector, const Vector3 &normal, double etaiOverEtat ) const{
+            double cosTheta = std::fmin( dot( -vector, normal ), 1.0 );
+            Vector3 rOutPerpendicular = etaiOverEtat * ( vector + cosTheta * normal );
+            Vector3 rOutParallel = -std::sqrt( std::fabs( 1.0 - rOutPerpendicular.lengthSquared())) * normal;
+
+            return rOutPerpendicular + rOutParallel;
+        }
+
+        Vector3 reflected( const Vector3 & vector, const Vector3 &normal ) const{
+            return vector - 2 * dot( vector, normal ) * normal;
+        }
+
+        double reflectance( double cosine, double RI ) const{
+            double r0 = ( 1 - RI ) / ( 1 + RI );
+            r0 = r0 * r0;
+            return r0 + ( 1 - r0 ) * std::pow( ( 1 - cosine ), 5 );
+        }
+
+        bool scatter( const Ray &ray, Color3 &attenuation, Ray &scattered, IntersectionManager &intersectionManager ) const override {
+            attenuation = Color3( 1.0, 1.0, 1.0 );
+            double rI = intersectionManager.frontFace ? ( 1.0 / refractionIndex ) : refractionIndex;
+
+            Vector3 unitDirection = unitVector( ray.direction() );
+            
+            double cosTheta = std::fmin( dot( -unitDirection, intersectionManager.normal ), 1.0 );
+            double sinTheta = std::sqrt( 1.0 - cosTheta * cosTheta );
+
+            bool cannotRefract = rI * sinTheta > 1.0;
+            Vector3 direction;
+
+            if( cannotRefract || reflectance( cosTheta, rI ) > generateRandomNumber() ){
+                direction = reflected( unitDirection, intersectionManager.normal );
+            }
+            else{
+                direction = refract( unitDirection, intersectionManager.normal, rI );
+            }
+
+            scattered = Ray( intersectionManager.point, direction );
+            return true;
+        } 
+
+    private:
+        double refractionIndex;
+};
+
 #endif
--- main.cpp
+++ main.cpp
@@ -18,9 +18,10 @@ int main(){
 
     World world;
 
-    world.add( make_shared< Sphere > ( Point3( 0, 0, -1 ), 0.5, make_shared< Diffuse >( Color3( 1.0, 0.0, 0.0 ) )));
-    world.add( make_shared< Sphere > ( Point3( -1, 0, -1 ), 0.5, make_shared< Diffuse >( Color3( 0.4, 0.4, 0.4 )  )));
-    world.add( make_shared< Sphere > ( Point3( 1, 0, -1 ), 0.5, make_shared< Metal >( Color3( 1.0, 1.0, 1.0 ), 0.5  )));
+    world.add( make_shared< Sphere > ( Point3( -1, 0, -1 ), 0.5, make_shared< Dielectric >( 1.50 )));
+    world.add( make_shared< Sphere > ( Point3( -1, 0, -1 ), 0.4, make_shared< Dielectric >( 1.00 / 1.50 )));
+    world.add( make_shared< Sphere > ( Point3( 0, 0, -1 ), 0.5, make_shared< Diffuse >( Color3( 0.4, 0.4, 0.4 )  )));
+    world.add( make_shared< Sphere > ( Point3( 1, 0, -1 ), 0.5, make_shared< Metal >( Color3( 1.0, 1.0, 1.0 ), 0.0  )));
 
     world.add( make_shared< Sphere > ( Point3( 0, -100.5, -1 ), 100, make_shared< Diffuse >( Color3( 0.4, 0.4, 0.4 )  )));
 
